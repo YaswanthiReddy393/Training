8160ad111e1f9fbb9a37b0440581527dba5ec077
YaswanthiReddy393 <work.yashwanthireddynandyala@gmail.com>
2025-01-06 22:29:42 +0530

    Pushing Datatypes files

---
+++ Day1/variable.js
@@ -0,0 +1,32 @@
+console.log("Start");
+
+//global
+var a = 10;
+console.log(a);
+
+//script
+let b = 20;
+console.log(b);
+
+const c = 30;
+console.log(c);
+
+//block
+{
+    var d = 40;
+    console.log(d);
+
+    let b = 20;
+    console.log(b);
+
+    const c = 30;
+    console.log(c);
+
+    
+}
+
+console.log("End");
+
+
+
+
---
+++ Day2/datatype.js
@@ -0,0 +1,22 @@
+const str1 = "Yashwanthi"
+let str2 = 'Reddy'
+var str3 = `Nandyala`
+console.log(str1, str2, str3);
+
+ const num1 = 19
+ let num2 = 12.89
+ var num3 = 10e5
+ console.log(num1, num2, num3);
+
+const bool1 = true
+var bool2 = false
+console.log(bool1, bool2, typeof(bool1, bool2));
+
+let a
+console.log(a, typeof(a));
+
+let b = null;
+console.log(b, typeof(b));
+
+
+
