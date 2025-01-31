1752561ed2c9d6918573295533578766d2847165
YaswanthiReddy393 <work.yashwanthireddynandyala@gmail.com>
2025-01-31 21:25:08 +0530

    Pushing Modules code

---
+++ Day18/App.js
@@ -0,0 +1,14 @@
+// import x from "./Modules.js";
+// console.log(x);
+import x,{user,users,obj} from "./Modules.js";
+console.log(user);
+console.log(users);
+users.map(user =>{
+    console.log(user);
+    
+});
+console.log(obj.name);
+console.log(x);
+
+
+
---
+++ Day18/Modules.js
@@ -0,0 +1,16 @@
+// let x = 10;
+// export default x;
+
+ export let user = "Yashu";
+let users = ["Bannu","Sharon","Yashu"];
+let obj = {
+    name:"Yashu",
+    company:"Amazon"
+}
+export {
+    users,
+    obj
+};
+
+let x = 10;
+export default x;
